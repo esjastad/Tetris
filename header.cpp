4f67646b6ea9ca83d240ae46c0e7a6061c766be3
esjastad <esjastad@gmail.com>
2017-05-17 17:48:20 -0700

    tetrisclone redo

--- header.cpp
+++ header.cpp
@@ -1,101 +1,331 @@
 #include "header.h"
 
-data::data()
+background::background()
 {
+    borderTexture.loadFromFile("borders.png");
+    borderTexture.setSmooth(true);
+    border.setTexture(borderTexture);
+}
+
+shape::shape()
+{
+    flag = 0;
+    blockTexture.loadFromFile("block.png");
+    blockTexture.setSmooth(true);
+    color = rand()%4+1;
+    sf::Color temporary;
+    if (color == 1)
+        temporary = sf::Color::Blue;
+    if (color == 2)
+        temporary = sf::Color::Green;
+    if (color == 3)
+        temporary = sf::Color::Yellow;
+    if (color == 4)
+        temporary = sf::Color::Red;
+
+    for(int i=0;i<4;++i)
+    {
+        block[i].setTexture(blockTexture);
+        block[i].setColor(temporary);
+    }
+
+    position = sf::Vector2f(420,0);
     srand(time(NULL));
-    gen = rand()%7+1;
-    L = 340;
-    R = 640;
-    B = 800;
+    gen = 1; //rand()%7+1;
+
+    if (gen==1) //stick
+    {
+            block[0].setPosition(sf::Vector2f(position.x,position.y-80));
+            block[1].setPosition(sf::Vector2f(position.x,position.y-120));
+            block[2].setPosition(sf::Vector2f(position.x,position.y-40));
+            block[3].setPosition(sf::Vector2f(position.x,position.y));
+    }
+
+    if (gen==2)
+    {
+            block[0].setPosition(position);
+            block[1].setPosition(sf::Vector2f(position.x,position.y-40));
+            block[2].setPosition(sf::Vector2f(position.x+40,position.y-40));
+            block[3].setPosition(sf::Vector2f(position.x+40,position.y));
+    }
+
+    if (gen==3)
+    {
+            block[0].setPosition(sf::Vector2f(position.x,position.y-40));
+            block[1].setPosition(sf::Vector2f(position.x,position.y));
+            block[2].setPosition(sf::Vector2f(position.x,position.y-80));
+            block[3].setPosition(sf::Vector2f(position.x+40,position.y-40));
+    }
+
+    if (gen==4)
+    {
+            block[0].setPosition(sf::Vector2f(position.x,position.y-40));
+            block[1].setPosition(sf::Vector2f(position.x,position.y-80));
+            block[2].setPosition(sf::Vector2f(position.x,position.y));
+            block[3].setPosition(sf::Vector2f(position.x+40,position.y));
+    }
+
+    if(gen==5)
+    {
+            block[0].setPosition(sf::Vector2f(position.x+40,position.y-40));
+            block[1].setPosition(sf::Vector2f(position.x+40,position.y-80));
+            block[2].setPosition(sf::Vector2f(position.x+40,position.y));
+            block[3].setPosition(sf::Vector2f(position.x,position.y));
+    }
+    if(gen==6)
+    {
+            block[0].setPosition(sf::Vector2f(position.x,position.y-40));
+            block[1].setPosition(sf::Vector2f(position.x,position.y-80));
+            block[2].setPosition(sf::Vector2f(position.x+40,position.y-40));
+            block[3].setPosition(sf::Vector2f(position.x+40,position.y));
+    }
+    if(gen==7)
+    {
+            block[0].setPosition(sf::Vector2f(position.x,position.y-40));
+            block[1].setPosition(sf::Vector2f(position.x,position.y));
+            block[2].setPosition(sf::Vector2f(position.x+40,position.y-40));
+            block[3].setPosition(sf::Vector2f(position.x+40,position.y-80));
+    }
+}
+
+bool shape::movement(sf::Vector2f destination, char temp)
+{
+    bool invalid = false;
+    for (int i=0;i<4;++i)
+        if(block[i].getPosition().y == 800 && temp == 's' || block[i].getPosition().x == 260 && temp == 'a' || block[i].getPosition().x == 620 && temp == 'd' )
+            invalid = true;
+
+    if (!invalid)
+        for (int i=0;i<4;++i)
+            block[i].move(destination);
+    position = position + destination;
+    return invalid;
+}
+
+void shape::rotater()
+{
 
     if (gen == 1)
     {
-        if (!grid.load("block.png", sf::Vector2u(40, 40), line, 4, 4))
-            return ;
-                    grid.setPosition(500,-40);
-        grid.setOrigin(80,120);
-
-    }
-
-    if (gen == 2)
-    {
-        if (!grid.load("block.png", sf::Vector2u(40, 40), block, 4, 4))
-            return ;
-                    grid.setPosition(500,-40);
-        grid.setOrigin(80,120);
-    }
-
-    if (gen == 3)
-    {
-        if (!grid.load("block.png", sf::Vector2u(40, 40), tshape, 4, 4))
-            return ;
-                    grid.setPosition(500,-40);
-        grid.setOrigin(80,120);
-    }
-
-    if (gen == 4)
-    {
-        if (!grid.load("block.png", sf::Vector2u(40, 40), llshape, 4, 4))
-            return ;
-                    grid.setPosition(500,-40);
-        grid.setOrigin(80,120);
-    }
-
-    if (gen == 5)
-    {
-        if (!grid.load("block.png", sf::Vector2u(40, 40), rlshape, 4, 4))
-            return ;
-                    grid.setPosition(500,-40);
-        grid.setOrigin(80,120);
-    }
-
-    if (gen == 6)
-    {
-        if (!grid.load("block.png", sf::Vector2u(40, 40), zlshape, 4, 4))
-            return ;
-        grid.setPosition(500,-40);
-        grid.setOrigin(80,120);
-    }
-    if (gen == 7)
-    {
-        if (!grid.load("block.png", sf::Vector2u(40, 40), zrshape, 4, 4))
-            return ;
-        grid.setPosition(500,-40);
-        grid.setOrigin(80,120);
-    }
-}
-
-void data::rotater()
-{
-    if (gen == 6)
-    {
-
-        if (flag == false)
+        sf::Vector2f temp;
+        temp = block[0].getPosition();
+        if (flag == 0)
         {
-            grid.rotate(90);
-            grid.setOrigin(80,80);
-            flag = true;
-            if (grid.getPosition().x == L)
-            {
-                grid.setPosition((grid.getPosition().x+40), grid.getPosition().y);
-            }
-            L = L+40;
+            block[1].setPosition(sf::Vector2f(temp.x-40,temp.y));
+            block[2].setPosition(sf::Vector2f(temp.x+40,temp.y));
+            block[3].setPosition(sf::Vector2f(temp.x+80,temp.y));
+            flag = 1;
         }
         else
         {
-            grid.setOrigin(80,120);
-            grid.rotate(-90);
-            flag = false;
-            L=L-40;
+            block[1].setPosition(sf::Vector2f(temp.x,temp.y-40));
+            block[2].setPosition(sf::Vector2f(temp.x,temp.y+40));
+            block[3].setPosition(sf::Vector2f(temp.x,temp.y+80));
+            flag = 0;
+        }
+
+
+        for (int i=0;i<4;++i)
+        {
+            currentposition = block[i].getPosition();
+            if (currentposition.x < 260)
+                movement(sf::Vector2f(40,0),'d');
+            if (currentposition.x > 620)
+                movement(sf::Vector2f(-40,0),'a');
+            if (currentposition.y > 800)
+                movement(sf::Vector2f(0,-40),'a');
         }
     }
-    return;
-}
+    if (gen == 3)
+    {
 
-load::load()
-{
-    background.loadFromFile("borders.png");
-    background.setSmooth(true);
-    borders.setTexture(background);
-}
+        sf::Vector2f temp;
+        temp = block[0].getPosition();
 
+        if (flag == 0)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x+40,temp.y));
+            block[2].setPosition(sf::Vector2f(temp.x-40,temp.y));
+            block[3].setPosition(sf::Vector2f(temp.x,temp.y-40));
+
+            flag = 1;
+        }
+
+        else if (flag == 1)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x,temp.y+40));
+            block[2].setPosition(sf::Vector2f(temp.x,temp.y-40));
+            block[3].setPosition(sf::Vector2f(temp.x-40,temp.y));
+            flag = 2;
+        }
+
+        else if (flag == 2)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x-40,temp.y));
+            block[2].setPosition(sf::Vector2f(temp.x+40,temp.y));
+            block[3].setPosition(sf::Vector2f(temp.x,temp.y+40));
+            flag = 3;
+        }
+
+        else if (flag == 3)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x,temp.y+40));
+            block[2].setPosition(sf::Vector2f(temp.x,temp.y-40));
+            block[3].setPosition(sf::Vector2f(temp.x+40,temp.y));
+            flag = 0;
+        }
+
+    }
+    if (gen == 4)
+    {
+
+        sf::Vector2f temp;
+        temp = block[0].getPosition();
+
+        if (flag == 0)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x-40,temp.y));
+            block[2].setPosition(sf::Vector2f(temp.x+40,temp.y));
+            block[3].setPosition(sf::Vector2f(temp.x+40,temp.y-40));
+            flag = 1;
+        }
+
+        else if (flag == 1)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x,temp.y-40));
+            block[2].setPosition(sf::Vector2f(temp.x,temp.y+40));
+            block[3].setPosition(sf::Vector2f(temp.x-40,temp.y-40));
+            flag = 2;
+        }
+
+        else if (flag == 2)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x+40,temp.y));
+            block[2].setPosition(sf::Vector2f(temp.x-40,temp.y));
+            block[3].setPosition(sf::Vector2f(temp.x-40,temp.y+40));
+            flag = 3;
+        }
+
+        else if (flag == 3)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x,temp.y-40));
+            block[2].setPosition(sf::Vector2f(temp.x,temp.y+40));
+            block[3].setPosition(sf::Vector2f(temp.x+40,temp.y+40));
+            flag = 0;
+        }
+
+    }
+    if (gen == 5)
+    {
+
+        sf::Vector2f temp;
+        temp = block[0].getPosition();
+
+        if (flag == 0)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x-40,temp.y));
+            block[2].setPosition(sf::Vector2f(temp.x+40,temp.y));
+            block[3].setPosition(sf::Vector2f(temp.x+40,temp.y+40));
+            flag = 1;
+        }
+
+        else if (flag == 1)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x,temp.y+40));
+            block[2].setPosition(sf::Vector2f(temp.x,temp.y-40));
+            block[3].setPosition(sf::Vector2f(temp.x+40,temp.y-40));
+            flag = 2;
+        }
+
+        else if (flag == 2)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x+40,temp.y));
+            block[2].setPosition(sf::Vector2f(temp.x-40,temp.y));
+            block[3].setPosition(sf::Vector2f(temp.x-40,temp.y-40));
+            flag = 3;
+        }
+
+        else if (flag == 3)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x,temp.y+40));
+            block[2].setPosition(sf::Vector2f(temp.x,temp.y-40));
+            block[3].setPosition(sf::Vector2f(temp.x-40,temp.y+40));
+            flag = 0;
+        }
+
+    }
+    if (gen == 6)
+    {
+
+        sf::Vector2f temp;
+        temp = block[0].getPosition();
+
+        if (flag == 0)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x-40,temp.y));
+            block[2].setPosition(sf::Vector2f(temp.x,temp.y-40));
+            block[3].setPosition(sf::Vector2f(temp.x+40,temp.y-40));
+            flag = 3;
+        }
+
+        else if (flag == 1)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x,temp.y+40));
+            block[2].setPosition(sf::Vector2f(temp.x-40,temp.y));
+            block[3].setPosition(sf::Vector2f(temp.x-40,temp.y-40));
+            flag = 2;
+        }
+        else if (flag == 2)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x+40,temp.y));
+            block[2].setPosition(sf::Vector2f(temp.x,temp.y+40));
+            block[3].setPosition(sf::Vector2f(temp.x-40,temp.y+40));
+            flag = 3;
+        }
+
+        else if (flag == 3)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x,temp.y-40));
+            block[2].setPosition(sf::Vector2f(temp.x+40,temp.y));
+            block[3].setPosition(sf::Vector2f(temp.x+40,temp.y+40));
+            flag = 0;
+        }
+    }
+    if (gen == 7)
+    {
+
+        sf::Vector2f temp;
+        temp = block[0].getPosition();
+
+        if (flag == 0)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x+40,temp.y));
+            block[2].setPosition(sf::Vector2f(temp.x,temp.y-40));
+            block[3].setPosition(sf::Vector2f(temp.x-40,temp.y-40));
+            flag = 3;
+        }
+
+        else if (flag == 1)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x,temp.y-40));
+            block[2].setPosition(sf::Vector2f(temp.x-40,temp.y));
+            block[3].setPosition(sf::Vector2f(temp.x-40,temp.y+40));
+            flag = 2;
+        }
+        else if (flag == 2)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x-40,temp.y));
+            block[2].setPosition(sf::Vector2f(temp.x,temp.y+40));
+            block[3].setPosition(sf::Vector2f(temp.x+40,temp.y+40));
+            flag = 3;
+        }
+
+        else if (flag == 3)
+        {
+            block[1].setPosition(sf::Vector2f(temp.x,temp.y+40));
+            block[2].setPosition(sf::Vector2f(temp.x+40,temp.y));
+            block[3].setPosition(sf::Vector2f(temp.x+40,temp.y-40));
+            flag = 0;
+        }
+    }
+}
--- main.cpp
+++ main.cpp
@@ -2,43 +2,52 @@
 
 int main()
 {
-    // create the window
+
     sf::RenderWindow window(sf::VideoMode(1000, 900), "Tilemap");
-    load game;
-
-
-    // define the level with an array of tile indices
-
-
-    // create the tilemap from the level definition
-
+    background game;
     node * head = new node;
     node * current = head;
-    node * temp = NULL;
-    int counter = 1;
+    current->next = new node;
+    node * temp = current->next;
+
+    temp->data.movement(sf::Vector2f(350,190),'s');
+
+
+
+
 
     sf::Clock clock;
     sf::Time elapsed1 = clock.getElapsedTime();
+    bool next = false;
 
-    // run the main loop
+
+
+
+
     while (window.isOpen())
     {
+
         elapsed1 = clock.getElapsedTime();
         if (elapsed1.asSeconds() > 1)
         {
-            if (current->shape.grid.getPosition().y < 800)
-                current->shape.grid.move(0,40);
-            if (current->shape.grid.getPosition().y == 800)
+            next = current->data.movement(sf::Vector2f(0,40),'s');
+            if (next)
             {
-                temp = new node;
-                temp->next = NULL;
-                current->next = temp;
                 current = current->next;
-                ++counter;
+
+                current->data.movement(sf::Vector2f(-350,-190),'s');
+
+                delete head;
+                head = current;
+                current->next = new node;
+                temp = current->next;
+
+
+                temp->data.movement(sf::Vector2f(350,190),'s');
             }
-
-
             clock.restart();
+
+            std::cout << "\n\n" << current->data.block[0].getPosition().x;
         }
 
         // handle events
@@ -47,67 +56,44 @@ int main()
         {
             if(event.type == sf::Event::Closed)
             {
-                /*if (head->next)
-                {
-                    while (head->next)
-                    {
-                        current = head->next;
-                        delete head;
-                        head = current;
-                    }
-                //    delete head;
-                //    head = NULL;
-                }
-                else
-                    delete head;
-                head = NULL;*/
                 window.close();
             }
 
-
             if(event.type == sf::Event::KeyPressed)
             {
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && current->shape.grid.getPosition().x > current->shape.L)
-                    current->shape.grid.move(-40, 0);
-
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && current->shape.grid.getPosition().x < current->shape.R)
-                    current->shape.grid.move(40, 0);
-
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && current->shape.grid.getPosition().y < current->shape.B)
-                    current->shape.grid.move(0, 40);
-
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+                {
+                    current->data.movement(sf::Vector2f(0,40),'s');
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+                {
+                    current->data.movement(sf::Vector2f(-40,0),'a');
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+                {
+                    current->data.movement(sf::Vector2f(40,0),'d');
+                }
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-                    current->shape.rotater();
+                {
+                    current->data.rotater();
+                }
 
             }
+
         }
 
-        // draw the map
+
         window.clear();
-        temp = head;
-        if (temp->next)
-        {
-            for(int i = 0; i < counter; ++i)
-            {
-                window.draw(temp->shape.grid);
-                temp = temp->next;
-            }
-        }
 
-        window.draw(current->shape.grid);
-        window.draw(game.borders);
+        for(int i=0;i<4;++i)
+        {
+            window.draw(current->data.block[i]);
+            window.draw(temp->data.block[i]);
+        }
+        window.draw(game.border);
         window.display();
     }
 
-    for (int i = 0; i < counter; ++i)
-    {
-        if (head)
-        {
-            temp = head->next;
-            delete head;
-            head = temp;
-        }
-    }
 
     return 0;
 }
